7deaba5853d06faaf06bf1ff37a2dec3d4e050df
IdealistCat <mattallan.h@gmail.com>
2025-01-18 21:13:36 -0600

    extensions_and_folder var

--- file_organizer.py
+++ file_organizer.py
@@ -1,2 +1,14 @@
 # This is a fun little script by Sphis_Sinco
 # 1.0 - []
+
+# this is a quick little dictionary to keep track of what files go where
+extensions_and_folder = {
+    "txt": "Data",
+    "json": "Data",
+    "xml": "Data",
+    "png": "Images/PNG",
+    "jpeg": "Images/JPEG",
+    "wav": "Sounds/WAV",
+    "mp3": "Sounds/MP3",
+    "ogg": "Sounds/OGG",
+}
